bce7395a5bb6db35c562c4237ffb622eb6e09d9a
Juliano R. Barbosa <rocha.juliano.b@gmail.com>
2019-03-28 09:21:10 -0300

    add deadzone and mouseSpeed parameters

---
+++ mouse-controller.js
@@ -0,0 +1,32 @@
+var robot = require("robotjs");
+
+function mouseController(){
+    var mouseDelay = 2;
+    var deadZoneSize = 3;
+    var mouseSpeed = 1;
+    
+    robot.setMouseDelay(mouseDelay);
+
+    module.exports.moveMouse = function(deltaX,deltaY){
+        var mouse = robot.getMousePos();
+
+        if (Math.abs(deltaX) < deadZoneSize){
+            deltaX = 0;
+        }
+        if (Math.abs(deltaY) < deadZoneSize){
+            deltaY = 0;
+        }
+
+        robot.moveMouse(mouse.x + -deltaX*mouseSpeed, mouse.y + -deltaY*mouseSpeed);
+    }
+
+    module.exports.setMouseDeadzone = function(deadZoneValue){
+        deadZoneSize = deadZoneValue;
+    }
+
+    module.exports.setMouseSpeed = function(speedValue){
+        mouseSpeed = speedValue;
+    }
+}
+
+mouseController();
